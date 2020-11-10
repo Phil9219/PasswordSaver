8ccc08ab682f4f14048a5c14721bf0d68508ac0b
Phil9219 <philipp.fell@yahoo.de>
2020-11-10 11:53:05 +0100

    Add inquirer

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 console.log("wifi=123");
+
+process.argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`);
+});
+
+const args = process.argv.slice(2);
+const passwordName = args[0];
+console.log;
+
+if (passwordName === "phil") {
+  console.log("Your PW is ppilihp");
+} else {
+  console.log("access denied");
+}
+
+const inquirer = require("inquirer");
+
+var questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What's your name?",
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  console.log(`Hi ${answers["name"]}!`);
+});
